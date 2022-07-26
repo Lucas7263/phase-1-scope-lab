5b8c71117568f0633e2f349fa2209b62ef07dc4c
Lucas <lucaswoodyard@yahoo.com>
2022-07-25 19:05:47 -0500

    commit tree 313e2fc47b7de8371efe5396d67e893d53e931e8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer = 'not bob'
+
+function upperCaseCustomerName() {
+    
+    result = window.customerName.toUpperCase();
+    window.customerName = result
+    
+}
+
+function setBestCustomer() {
+    
+   result =  window.bestCustomer = 'not bob' 
+    window.bestCustomer = result
+}
+
+setBestCustomer()
+
+
+
+
+
